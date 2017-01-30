317b0d05d18cac2eb83cbd651d1356aca3017416
Melissa O'Connor <moconnor@factset.com>
2017-01-29 21:35:53 -0500

    fix(server): port

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.use([
   express.static(__dirname + '/img')
 ]);
 
-// const port = process.env.PORT;
-const port = 5000;
+const port = process.env.PORT;
+// const port = 5000;
 app.listen(port, () => console.log('server listening on port ' + port));
